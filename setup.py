5990c3c897b33087b07dde399828155642fc69ce
cafehaine <kilian.guillaume@gmail.com>
2020-09-02 10:24:00 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.md').read()
 
 setup(
     name="xontrib-xlsd",
-    version="0.0.2",
+    version="0.0.3",
     license="GPLv3",
     url="https://github.com/cafehaine/xontrib-xlsd",
     description="An improved ls for xonsh, inspired by lsd",
